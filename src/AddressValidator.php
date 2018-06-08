dafbc013963f46022102c102602f2812f9dfcea3
Andrzej Budzanowski <andrzej.budzanowski@l4a-soft.com>
2018-06-08 12:12:23 +0200

    Added method for generating correct checksumed address

--- src/AddressValidator.php
+++ src/AddressValidator.php
@@ -39,4 +39,29 @@ class AddressValidator
 
         return self::ADDRESS_VALID;
     }
+
+    public static function getCanonicalAddress(string $address): ?string
+    {
+        if (!preg_match('/^0x[a-fA-F0-9]{40}$/', $address)) {
+            return null;
+        }
+
+        $address = substr($address, 2);
+        $addressHash = Keccak::hash(strtolower($address), 256);
+        $addressArray = str_split($address);
+        $addressHashArray = str_split($addressHash);
+
+        $ret = '';
+
+        for ($i = 0; $i < 40; $i++) {
+            // the nth letter should be uppercase if the nth digit of casemap is 1
+            if (intval($addressHashArray[$i], 16) > 7) {
+                $ret .= strtoupper($addressArray[$i]);
+            } else /*if (intval($addressHashArray[$i], 16) <= 7)*/ {
+                $ret .= strtolower($addressArray[$i]);
+            }
+        }
+
+        return '0x' . $ret;
+    }
 }
--- tests/AddressValidatorTest.php
+++ tests/AddressValidatorTest.php
@@ -15,4 +15,13 @@ class AddressValidatorTest extends TestCase
         $this->assertEquals(AddressValidator::ADDRESS_INVALID, AddressValidator::isValid('0x3b6969198Aa794AAc16feEEa051a345BD806A16eAAc'));
         $this->assertEquals(AddressValidator::ADDRESS_VALID, AddressValidator::isValid('0x3b6969198aa794aac16feeea051a345bd806a16e'));
     }
+
+    public function testIsCanonical()
+    {
+        $this->assertEquals('0x3b6969198Aa794AAc16feEEa051a345BD806A16e', AddressValidator::getCanonicalAddress('0x3b6969198Aa794AAc16feEEa051a345BD806A16e'));
+        $this->assertEquals('0x3b6969198Aa794AAc16feEEa051a345BD806A16e', AddressValidator::getCanonicalAddress('0x3b6969198aa794AAc16feEEa051a345BD806A16e'));
+        $this->assertNull(AddressValidator::getCanonicalAddress('0x3b6969198aa794AAc16feEEa051a'));
+        $this->assertNull(AddressValidator::getCanonicalAddress('0x3b6969198Aa794AAc16feEEa051a345BD806A16eAAc'));
+        $this->assertEquals('0x3b6969198Aa794AAc16feEEa051a345BD806A16e', AddressValidator::getCanonicalAddress('0x3b6969198aa794aac16feeea051a345bd806a16e'));
+    }
 }

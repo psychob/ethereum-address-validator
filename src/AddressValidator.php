80803040c74cb864165a76868d52bc139a729568
Andrzej Budzanowski <andrzej.budzanowski@l4a-soft.com>
2018-06-08 11:09:59 +0200

    initial commit

---
+++ src/AddressValidator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace PsychoB\Ethereum;
+
+use kornrunner\Keccak;
+
+class AddressValidator
+{
+    public const ADDRESS_VALID = 0;
+    public const ADDRESS_CHECKSUM_INVALID = 1;
+    public const ADDRESS_INVALID = 2;
+
+    public static function isValid(string $address): int
+    {
+        if (!preg_match('/^0x[a-fA-F0-9]{40}$/', $address)) {
+            return self::ADDRESS_INVALID;
+        }
+
+        if (preg_match('/^0x[a-f0-9]{40}$/', $address) || preg_match('/^0x[A-F0-9]{40}$/', $address)) {
+            return self::ADDRESS_VALID;
+        }
+
+        return self::validateChecksumAddress(substr($address, 2));
+    }
+
+    private static function validateChecksumAddress($address): int
+    {
+        $addressHash = Keccak::hash(strtolower($address), 256);
+        $addressArray = str_split($address);
+        $addressHashArray = str_split($addressHash);
+
+        for ($i = 0; $i < 40; $i++) {
+            // the nth letter should be uppercase if the nth digit of casemap is 1
+            if ((intval($addressHashArray[$i], 16) > 7 && strtoupper($addressArray[$i]) !== $addressArray[$i]) ||
+                (intval($addressHashArray[$i], 16) <= 7 && strtolower($addressArray[$i]) !== $addressArray[$i])) {
+                return self::ADDRESS_CHECKSUM_INVALID;
+            }
+        }
+
+        return self::ADDRESS_VALID;
+    }
+}
---
+++ tests/AddressValidatorTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\PsychoB\Ethereum;
+
+use PHPUnit\Framework\TestCase;
+use PsychoB\Ethereum\AddressValidator;
+
+class AddressValidatorTest extends TestCase
+{
+    public function testIsValid()
+    {
+        $this->assertEquals(AddressValidator::ADDRESS_VALID, AddressValidator::isValid('0x3b6969198Aa794AAc16feEEa051a345BD806A16e'));
+        $this->assertEquals(AddressValidator::ADDRESS_CHECKSUM_INVALID, AddressValidator::isValid('0x3b6969198aa794AAc16feEEa051a345BD806A16e'));
+        $this->assertEquals(AddressValidator::ADDRESS_INVALID, AddressValidator::isValid('0x3b6969198aa794AAc16feEEa051a'));
+        $this->assertEquals(AddressValidator::ADDRESS_INVALID, AddressValidator::isValid('0x3b6969198Aa794AAc16feEEa051a345BD806A16eAAc'));
+        $this->assertEquals(AddressValidator::ADDRESS_VALID, AddressValidator::isValid('0x3b6969198aa794aac16feeea051a345bd806a16e'));
+    }
+}

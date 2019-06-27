40d78eaca6bb8a24166f07bc55a1b4a9542bff3c
Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
2019-06-27 10:37:47 +0200

    Add file comment, update formatting

--- src/AddressValidator.php
+++ src/AddressValidator.php
@@ -1,92 +1,99 @@
 <?php
+    //
+    // psychob/ethereum-address-validator
+    // (c) 2018 - 2019 RGB Lighthouse <https://rgblighthouse.pl>
+    // (c) 2018 - 2019 Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
+    // --
+    // See LICENSE for license information.
+    //
 
-namespace PsychoB\Ethereum;
+    namespace PsychoB\Ethereum;
 
-use kornrunner\Keccak;
+    use kornrunner\Keccak;
 
-class AddressValidator
-{
-    /**
-     * String is a valid Ethereum or Etherum Classic address
-     */
-    public const ADDRESS_VALID = 0;
-
-    /**
-     * String have valid address, but capitalization is incorrect.
-     */
-    public const ADDRESS_CHECKSUM_INVALID = 1;
-
-    /**
-     * String is not an Ethereum or Ethereum Classic address
-     */
-    public const ADDRESS_INVALID = 2;
-
-    /**
-     * Check if string is Ethereum or Ethereum Classic address
-     *
-     * @param string $address
-     *
-     * @return int
-     */
-    public static function isValid(string $address): int
+    class AddressValidator
     {
-        if (!preg_match('/^0x[a-fA-F0-9]{40}$/', $address)) {
-            return self::ADDRESS_INVALID;
+        /**
+         * String is a valid Ethereum or Etherum Classic address
+         */
+        public const ADDRESS_VALID = 0;
+
+        /**
+         * String have valid address, but capitalization is incorrect.
+         */
+        public const ADDRESS_CHECKSUM_INVALID = 1;
+
+        /**
+         * String is not an Ethereum or Ethereum Classic address
+         */
+        public const ADDRESS_INVALID = 2;
+
+        /**
+         * Check if string is Ethereum or Ethereum Classic address
+         *
+         * @param string $address
+         *
+         * @return int
+         */
+        public static function isValid(string $address): int
+        {
+            if (!preg_match('/^0x[a-fA-F0-9]{40}$/', $address)) {
+                return self::ADDRESS_INVALID;
+            }
+
+            if (preg_match('/^0x[a-f0-9]{40}$/', $address) || preg_match('/^0x[A-F0-9]{40}$/', $address)) {
+                return self::ADDRESS_VALID;
+            }
+
+            return self::validateChecksumAddress(substr($address, 2));
         }
 
-        if (preg_match('/^0x[a-f0-9]{40}$/', $address) || preg_match('/^0x[A-F0-9]{40}$/', $address)) {
+        private static function validateChecksumAddress($address): int
+        {
+            $addressHash = Keccak::hash(strtolower($address), 256);
+            $addressArray = str_split($address);
+            $addressHashArray = str_split($addressHash);
+
+            for ($i = 0; $i < 40; $i++) {
+                // the nth letter should be uppercase if the nth digit of casemap is 1
+                if ((intval($addressHashArray[$i], 16) > 7 && strtoupper($addressArray[$i]) !== $addressArray[$i]) ||
+                    (intval($addressHashArray[$i], 16) <= 7 && strtolower($addressArray[$i]) !== $addressArray[$i])) {
+                    return self::ADDRESS_CHECKSUM_INVALID;
+                }
+            }
+
             return self::ADDRESS_VALID;
         }
 
-        return self::validateChecksumAddress(substr($address, 2));
-    }
-
-    private static function validateChecksumAddress($address): int
-    {
-        $addressHash = Keccak::hash(strtolower($address), 256);
-        $addressArray = str_split($address);
-        $addressHashArray = str_split($addressHash);
-
-        for ($i = 0; $i < 40; $i++) {
-            // the nth letter should be uppercase if the nth digit of casemap is 1
-            if ((intval($addressHashArray[$i], 16) > 7 && strtoupper($addressArray[$i]) !== $addressArray[$i]) ||
-                (intval($addressHashArray[$i], 16) <= 7 && strtolower($addressArray[$i]) !== $addressArray[$i])) {
-                return self::ADDRESS_CHECKSUM_INVALID;
+        /**
+         * Generate address with checksum from non-checksumed address.
+         *
+         * @param string $address
+         *
+         * @return string|null
+         */
+        public static function getCanonicalAddress(string $address): ?string
+        {
+            if (!preg_match('/^0x[a-fA-F0-9]{40}$/', $address)) {
+                return NULL;
             }
-        }
 
-        return self::ADDRESS_VALID;
-    }
+            $address = substr($address, 2);
+            $addressHash = Keccak::hash(strtolower($address), 256);
+            $addressArray = str_split($address);
+            $addressHashArray = str_split($addressHash);
 
-    /**
-     * Generate address with checksum from non-checksumed address.
-     *
-     * @param string $address
-     *
-     * @return string|null
-     */
-    public static function getCanonicalAddress(string $address): ?string
-    {
-        if (!preg_match('/^0x[a-fA-F0-9]{40}$/', $address)) {
-            return null;
-        }
+            $ret = '';
 
-        $address = substr($address, 2);
-        $addressHash = Keccak::hash(strtolower($address), 256);
-        $addressArray = str_split($address);
-        $addressHashArray = str_split($addressHash);
-
-        $ret = '';
-
-        for ($i = 0; $i < 40; $i++) {
-            // the nth letter should be uppercase if the nth digit of casemap is 1
-            if (intval($addressHashArray[$i], 16) > 7) {
-                $ret .= strtoupper($addressArray[$i]);
-            } else /*if (intval($addressHashArray[$i], 16) <= 7)*/ {
-                $ret .= strtolower($addressArray[$i]);
+            for ($i = 0; $i < 40; $i++) {
+                // the nth letter should be uppercase if the nth digit of casemap is 1
+                if (intval($addressHashArray[$i], 16) > 7) {
+                    $ret .= strtoupper($addressArray[$i]);
+                } else /*if (intval($addressHashArray[$i], 16) <= 7)*/ {
+                    $ret .= strtolower($addressArray[$i]);
+                }
             }
-        }
 
-        return '0x' . $ret;
+            return '0x' . $ret;
+        }
     }
-}
--- tests/AddressValidatorTest.php
+++ tests/AddressValidatorTest.php
@@ -1,71 +1,78 @@
 <?php
+    //
+    // psychob/ethereum-address-validator
+    // (c) 2018 - 2019 RGB Lighthouse <https://rgblighthouse.pl>
+    // (c) 2018 - 2019 Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
+    // --
+    // See LICENSE for license information.
+    //
 
-namespace Tests\PsychoB\Ethereum;
+    namespace Tests\PsychoB\Ethereum;
 
-use PHPUnit\Framework\TestCase;
-use PsychoB\Ethereum\AddressValidator;
+    use PHPUnit\Framework\TestCase;
+    use PsychoB\Ethereum\AddressValidator;
 
-class AddressValidatorTest extends TestCase
-{
-    public function providerTestIsValid()
+    class AddressValidatorTest extends TestCase
     {
-        return [
-            ['0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85'], // with checksum
-            ['0xa477941c7aad6536f175ef123bf9eed6f82a4c85'], // without checksum, lower case
-            ['0xA477941C7AAD6536F175EF123BF9EED6F82A4C85'], // without checksum, upper case
-        ];
-    }
+        public function providerTestIsValid()
+        {
+            return [
+                ['0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85'], // with checksum
+                ['0xa477941c7aad6536f175ef123bf9eed6f82a4c85'], // without checksum, lower case
+                ['0xA477941C7AAD6536F175EF123BF9EED6F82A4C85'], // without checksum, upper case
+            ];
+        }
 
-    /** @dataProvider providerTestIsValid */
-    public function testIsValid(string $address)
-    {
-        $this->assertEquals(AddressValidator::ADDRESS_VALID, AddressValidator::isValid($address));
-    }
+        /** @dataProvider providerTestIsValid */
+        public function testIsValid(string $address)
+        {
+            $this->assertEquals(AddressValidator::ADDRESS_VALID, AddressValidator::isValid($address));
+        }
 
-    public function providerTestIsInValidChecksum()
-    {
-        return [
-            ['0xA477941c7aad6536f175ef123bf9eeD6F82A4c85'], // invalid checksum, capitalisation
-            ['0xA477941c7aFd6536f175ef123bf9eeD6F82A4c85'], // invalid checksum, wrong address
-        ];
-    }
+        public function providerTestIsInValidChecksum()
+        {
+            return [
+                ['0xA477941c7aad6536f175ef123bf9eeD6F82A4c85'], // invalid checksum, capitalisation
+                ['0xA477941c7aFd6536f175ef123bf9eeD6F82A4c85'], // invalid checksum, wrong address
+            ];
+        }
 
-    /** @dataProvider providerTestIsInValidChecksum */
-    public function testIsInValidChecksum(string $address)
-    {
-        $this->assertEquals(AddressValidator::ADDRESS_CHECKSUM_INVALID, AddressValidator::isValid($address));
-    }
+        /** @dataProvider providerTestIsInValidChecksum */
+        public function testIsInValidChecksum(string $address)
+        {
+            $this->assertEquals(AddressValidator::ADDRESS_CHECKSUM_INVALID, AddressValidator::isValid($address));
+        }
 
-    public function providerTestIsInvalid()
-    {
-        return [
-            ['0xA477941c7AAD6536f175ef123bf9eeD6F82A4c8'], // too short address
-            ['0xA477941c7AAD6536f175ef123bf9eeD6F82A4c856'], // address too long
-            [''],
-        ];
-    }
+        public function providerTestIsInvalid()
+        {
+            return [
+                ['0xA477941c7AAD6536f175ef123bf9eeD6F82A4c8'], // too short address
+                ['0xA477941c7AAD6536f175ef123bf9eeD6F82A4c856'], // address too long
+                [''],
+            ];
+        }
 
-    /** @dataProvider providerTestIsInvalid */
-    public function testIsInvalid(string $address)
-    {
-        $this->assertEquals(AddressValidator::ADDRESS_INVALID, AddressValidator::isValid($address));
-    }
+        /** @dataProvider providerTestIsInvalid */
+        public function testIsInvalid(string $address)
+        {
+            $this->assertEquals(AddressValidator::ADDRESS_INVALID, AddressValidator::isValid($address));
+        }
 
-    public function providerTestGetCanonical()
-    {
-        return [
-            ['0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85', '0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85'],
-            ['0xa477941c7aad6536f175ef123bf9eed6f82a4c85', '0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85'],
-            ['0xA477941C7AAD6536F175EF123BF9EED6F82A4C85', '0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85'],
-            ['0xA477941c7aad6536f175ef123bf9eeD6F82A4c85', '0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85'],
-            ['0xA477941c7aFd6536f175ef123bf9eeD6F82A4c85', '0xa477941c7aFD6536f175ef123bf9eed6f82A4C85'],
-            ['', null],
-        ];
-    }
+        public function providerTestGetCanonical()
+        {
+            return [
+                ['0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85', '0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85'],
+                ['0xa477941c7aad6536f175ef123bf9eed6f82a4c85', '0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85'],
+                ['0xA477941C7AAD6536F175EF123BF9EED6F82A4C85', '0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85'],
+                ['0xA477941c7aad6536f175ef123bf9eeD6F82A4c85', '0xA477941c7AAD6536f175ef123bf9eeD6F82A4c85'],
+                ['0xA477941c7aFd6536f175ef123bf9eeD6F82A4c85', '0xa477941c7aFD6536f175ef123bf9eed6f82A4C85'],
+                ['', NULL],
+            ];
+        }
 
-    /** @dataProvider providerTestGetCanonical */
-    public function testGetCanonical(string $address, $result)
-    {
-        $this->assertEquals($result, AddressValidator::getCanonicalAddress($address));
+        /** @dataProvider providerTestGetCanonical */
+        public function testGetCanonical(string $address, $result)
+        {
+            $this->assertEquals($result, AddressValidator::getCanonicalAddress($address));
+        }
     }
-}
